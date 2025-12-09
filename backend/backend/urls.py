0d8440d7080492f200a34c4052b27e00d29b5859
printnewcode <zenit200803@mail.ru>
2025-12-09 16:57:55 +0300

    Knopki Nizhney Paneli

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,4 +1,4 @@
-"""
+кк"""
 URL configuration for backend project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -12,11 +12,12 @@ Class-based views
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    2. dd a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import  include, path
 
 urlpatterns = [
+    path('', include('main.urls')),
     path('admin/', admin.site.urls),
 ]
