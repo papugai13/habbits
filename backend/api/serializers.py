bbf7a72538e2d64e3b9a9f1d8af33f47b39ee965
NurislamSalakhov <156327436+NurislamSalakhov@users.noreply.github.com>
2025-12-11 16:21:19 +0300

    Update serializers.py

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,6 +1,11 @@
 from rest_framework import serializers
-from . models import Date
+from . models import Date, UserAll
 
+
+class UserAllSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserAll
+        fields = '__all__'
 
 class DateSerializer(serializers.ModelSerializer):
     class Meta:
