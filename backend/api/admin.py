6725e2c9513cdace3e0600dd082355ca6533af21
printnewcode <zenit200803@mail.ru>
2025-12-23 17:59:59 +0300

    admin-panel

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from api.models import Habit, Date
 
-# Register your models here.
+
+class HabitAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'slug',
+    )
+    list_display_links = (
+        'name',
+    )
+    prepopulated_fields = {'slug': ('name',)}
+
+
+class DateAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+    )
+    search_fields = (
+        'name',
+        'description',
+    )
+    empty_value_display = (
+        '-пусто-'
+    )
+
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Habit, HabitAdmin)
+admin.site.register(Date, DateAdmin)
