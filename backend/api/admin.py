b398b27d8c9c203afaa303efaef487006c9a0e46
Nurislam <a123aa16rus@gmail.com>
2025-12-23 18:00:41 +0300

    2

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from api.models import UserAll
 
-# Register your models here.
+class UserAllAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'slug',
+        'age'
+    )
+    list_display_links = (
+        'name',
+    )
+    search_fields = (
+        'name',
+    )
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(UserAll, UserAllAdmin)
